d95abfcb187524131684562bd3ce43415292cfa5
sunsunarto <sunaryo24259@outlook.com>
2025-04-16 11:41:29 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ function App() {
 
   return (
     <div className="main">
-      <div className="con">
+      <div className="con pc">
         <div className="text">
-          <p className='subtext'>Kecerdasan visual</p><br/>
+          <p className='subtext'>Kecerdasan visual</p>
           <p className='title'>Lihat dunia dari lensa yang baru.</p>
           <p className='maintext'>Gunakan <span>kecerdasan visual</span><br/>melalui Kontrol Kamera untuk<br/>langsung mempelajari tentang<br/>semua hal yang Anda liat.<br/>Cukup <span>arahkan iPhone 16 Pro<br/>Anda untuk menemukan lebih</span><br/>atau berinteraksi dengan<br/>sesuatu di depan Anda. Cari<br/>tahu di mana tempat membeli<br/>barang baru yang Anda lihat,<br/>identifikasikan tanaman dan hewan, dan<br/>banyak lagi.</p>
+        </div>
+        <div className="img">
+          <img src="https://s3-alpha-sig.figma.com/img/2c92/727a/f508b52dbc762450046e9fa820612089?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ChJl0W9S1N5D~28cvJa0PrlotSxK7~t6UgynweuudzzJb-Bsu17I-4ao9QtSPMu~rmbD2uutH91UhYw7SIGaL547NQtTv~86juq4Bz6jcwSCcOhNQ3JwzPsrENT6txE1a7xwpIh7GPOp80laewiHpxidUmmpcdCVQ4cbEnydLTUtewB5NWLGh-4Za3HMPxgEC~kz6bomJw3GJgnHrU6LuvxWsphg4QU9-UABL3khztQ2rf2oyxpVpRG2L7CxzpS17F745-C1C3w-5~IuMp0OJLOFAOcFZYZYT1ANlUCfbpHGcGM5~~7Fg61fGo~6vMBCxqN2lh2-krGiPhafb1sXSg__" alt="" />
+        </div>
+      </div>
+      <div className="con mb">
+        <div className="text">
+          <p className='subtext'>Kecerdasan visual</p>
+          <p className='title'>Lihat dunia dari<br/>lensa yang baru.</p>
+          <p className='maintext'>Gunakan <span>kecerdasan visual</span> melalui Kontrol Kamera<br/>untuk langsung mempelajari tentang semua hal yang<br/>Anda liat. Cukup <span>arahkan iPhone 16 Pro Anda untuk<br/>menemukan lebih</span> atau berinteraksi dengan<br/>sesuatu di depan Anda. Cari tahu di mana tempat<br/>membeli barang baru yang Anda lihat,identifikasikan<br/>tanaman dan hewan, dan banyak lagi.</p>
         </div>
         <div className="img">
           <img src="https://s3-alpha-sig.figma.com/img/2c92/727a/f508b52dbc762450046e9fa820612089?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ChJl0W9S1N5D~28cvJa0PrlotSxK7~t6UgynweuudzzJb-Bsu17I-4ao9QtSPMu~rmbD2uutH91UhYw7SIGaL547NQtTv~86juq4Bz6jcwSCcOhNQ3JwzPsrENT6txE1a7xwpIh7GPOp80laewiHpxidUmmpcdCVQ4cbEnydLTUtewB5NWLGh-4Za3HMPxgEC~kz6bomJw3GJgnHrU6LuvxWsphg4QU9-UABL3khztQ2rf2oyxpVpRG2L7CxzpS17F745-C1C3w-5~IuMp0OJLOFAOcFZYZYT1ANlUCfbpHGcGM5~~7Fg61fGo~6vMBCxqN2lh2-krGiPhafb1sXSg__" alt="" />
